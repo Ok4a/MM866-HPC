517690f4c74c82cd0e965adfe0d627e57d69a164
Morten-Jensen <117636682+Morten-Jensen@users.noreply.github.com>
2024-12-13 13:38:11 +0100

    iuo

--- data_analysis.m
+++ data_analysis.m
@@ -1,9 +1,7 @@
-clc
-clear 
-close
+clc; clear; close;
 % Load data
 OMP_data = readmatrix("OMPdata_1733390325.csv");
-MPI_data = readmatrix("MPIdata_1733390385.csv");
+MPI_data = readmatrix("MPIdata_1733910289.csv");
 
 % Extract runtime data from OMP matrix. 8, 7, and 5 are for the numbers
 % N, numbers of core counts and number of runs respectively
@@ -31,33 +29,40 @@ N_vec = OMP_data(1: 8, 1);
 Core_vecOMP = 2.^(0: 6);
 Core_vecMPI = 2.^(1: 6);
 
-OMP_speedup_strong = avg_runtimeOMP(1, 1) ./ avg_runtimeOMP(1, 1: end);
-MPI_speedup_strong = avg_runtimeMPI(1, 1) ./ avg_runtimeMPI(1, 1: end);
+% Compute strong scale speedup for OpenMP and OpenMP
+OMP_speedup_strong = avg_runtimeOMP(:, 1) ./ avg_runtimeOMP(:, 1: end);
+MPI_speedup_strong = avg_runtimeMPI(:, 1) ./ avg_runtimeMPI(:, 1: end);
 
 % Strong scaling plots
 
 % OMP
 hold on
 plot(Core_vecOMP, OMP_speedup_strong, "-x");
+xticks(Core_vecOMP)
 line([1, 64], [1, 64])
+axis("tight")
 title("Strong Scaling OpenMP")
-legend("Real Speedup", "Ideal Speedup")
+legend("1024", "1472", "2048","2880", "4032",...
+    "5696", "8000", "11328","Ideal Speedup", "Location", "northwest")
 xlabel("Thread Count")
 ylabel("Speedup")
 hold off
-set(gcf, "Position", [100, 100, 1100, 700])
+set(gcf, "Position", [100, 100, 900, 500])
 close
 
 % MPI
 hold on
 plot(Core_vecMPI, MPI_speedup_strong, "-x");
-line([2, 64], [2, 64])
+xticks(Core_vecMPI)
+line([1, 64], [1, 64])
+axis("tight")
 title("Strong Scaling OpenMPI")
-legend("Real Speedup", "Ideal Speedup")
+legend("1024", "1472", "2048","2880", "4032",...
+    "5696", "8000", "11328","Ideal Speedup", "Location", "northwest")
 xlabel("Thread Count")
 ylabel("Speedup")
 hold off
-set(gcf, "Position", [100, 100, 1100, 700])
+set(gcf, "Position", [100, 100, 900, 500])
 close
 
 % Extract diagonal entries of avg_runtime as the workload
@@ -66,32 +71,23 @@ workloadMPI = diag(avg_runtimeMPI);
 
 % Compute speed up in relation to workload
 OMP_speedup_weak = workloadOMP(1) ./ workloadOMP;
-MPI_speedup_weak = workloadMPI(1) ./ workloadMPI;
+MPI_speedup_weak = (workloadMPI(1)) ./ workloadMPI;
 
 % Weak scaling plots
 
-% OMP
+% OMP and MPI
 hold on
 plot(Core_vecOMP, OMP_speedup_weak, "-x");
+plot(Core_vecMPI, MPI_speedup_weak, "--o");
+xticks(Core_vecOMP)
 line([1, 64], [1, 1])
-legend("Real Efficiency", "Ideal Efficiency")
-title("Weak Scaling OpenMP")
+legend("Efficiency OpenMP", "Efficiency OpenMPI", "Ideal Efficiency")
+axis("tight")
+title("Weak Scaling OpenMP and OpenMPI")
 xlabel("Thread Count")
 ylabel("Speedup")
 hold off
-set(gcf, "Position", [100, 100, 1100, 700])
-close
-
-% MPI
-hold on
-plot(Core_vecMPI, MPI_speedup_weak, "-x");
-line([2, 64], [1, 1]);
-legend("Real Efficiency", "Ideal Efficiency")
-title("Weak Scaling OpenMPI")
-xlabel("Thread Count")
-ylabel("Speedup")
-hold off
-set(gcf, "Position", [100, 100, 1100, 700])
+set(gcf, "Position", [100, 100, 900, 500])
 close
 
 % Errorbar plots
@@ -99,17 +95,19 @@ close
 % OMP
 errorbar(N_vec, log(avg_runtimeOMP), log(std_runtimeOMP))
 title("Average runtime with errorbars OpenMP")
-legend("1", "2", "4", "8", "16", "32", "64")
+%axis("tight")
+legend("1", "2", "4", "8", "16", "32", "64", "Location","northwest")
 xlabel("Grid side length")
 ylabel("Log runtime")
-set(gcf, "Position", [100, 100, 1100, 700])
+set(gcf, "Position", [100, 100, 900, 500])
 close
 
 % MPI
 errorbar(N_vec, log(avg_runtimeMPI), log(std_runtimeMPI))
 title("Average runtime with errorbars OpenMPI")
-legend("2", "4", "8", "16", "32", "64")
+%axis("tight")
+legend("2", "4", "8", "16", "32", "64", "Location","northwest")
 xlabel("Grid side length")
 ylabel("Log runtime")
-set(gcf, "Position", [100, 100, 1100, 700])
+set(gcf, "Position", [100, 100, 900, 500])
 close
---
+++ test_if_ass.m
@@ -0,0 +1,5 @@
+x = 3;
+
+if x = 4
+    disp(x)
+end
